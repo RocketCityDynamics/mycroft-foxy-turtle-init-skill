472c7b426c9b26724dea564174a7b8771806d8d6
Rocket City Dynamics <46632364+RocketCityDynamics@users.noreply.github.com>
2021-02-27 11:43:39 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ class MycroftFoxyTurtleInit(MycroftSkill):
     @intent_file_handler('init.turtle.foxy.mycroft.intent')
     def handle_init_turtle_foxy_mycroft(self, message):
         self.speak_dialog('init.turtle.foxy.mycroft')
-        subprocess.call((["ros2 run turtlesim turtlesim_node"]),shell=True)
+        subprocess.call(["ros2 run turtlesim turtlesim_node"],shell=True)
 
 def create_skill():
     return MycroftFoxyTurtleInit()
